5ee4d234e32381afad121bde696ced65cf11d737
adler <pitriadamasiruma@gmail.com>
2025-09-02 18:04:23 -0300

    First Commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,20 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import tseslint from 'typescript-eslint'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
 
-
-module.exports = {
-// ... outras configurações
-extends: [
-// ... outras extensões
-'eslint-config-prettier'
-],
-// ... resto do arquivo
-}
-
-
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooks.configs.recommended,
+  prettier, 
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    plugins: {
+      'react-refresh': reactRefresh,
     },
-  },
-])
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    }
+  }
+]
