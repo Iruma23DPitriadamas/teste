87ff55971ae7e891b36c33385417f8a682fc3adb
adler <pitriadamasiruma@gmail.com>
2025-12-01 10:42:44 -0300

    Feat material-ui-insert

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Login, Registration } from './pages'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>HOME</h1>} />
-        <Route path="/home" element={<h1>HOME</h1>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Registration />} />
         <Route path="/leads" element={<h1>LEADS</h1>} />
         <Route path="/perfil" element={<h1>PERFIL</h1>} />
       </Routes>
