9e515ac890376ce9729b7f22e8a795ec1cdfa4ae
adler <pitriadamasiruma@gmail.com>
2025-12-19 14:22:55 -0300

    Feat Create Themes

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -3,6 +3,8 @@ import type { DefaultTheme } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
     body, html {
+    background: ${(props) => props.theme.appBackground};
+    color: ${(props) => props.theme.appColor};
     margin: 0;
     padding: 0;
     font-family: "Inter", sans-serif;
